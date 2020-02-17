b3a9cc4c6894f03a245039fdd4d6ec0577d70519
gmonteeiro <gmonteeiro@gmail.com>
2020-02-17 19:47:46 -0300

    routes
    routes

--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth'
 import { auth } from 'firebase/app'
+import { Router } from '@angular/router'
+
+import {AlertController} from '@ionic/angular'
+
 
 @Component({
   selector: 'app-register',
@@ -13,7 +17,11 @@ export class RegisterPage implements OnInit {
 	password: string = ""
 	cpassword: string = ""
 
-  	constructor(public afAuth: AngularFireAuth) { }
+  	constructor(
+		public afAuth: AngularFireAuth,
+		public alert: AlertController,
+		public router: Router
+	) { }
 
 	ngOnInit() {
 	}
@@ -21,16 +29,29 @@ export class RegisterPage implements OnInit {
 	async register() {
 		const {username, password, cpassword} = this
 		if(password !== cpassword){
+			this.showAlert("Error!", "Passwords don't match")
 			return console.error("password don't match!");
 		}
 
 		try{
 			const res = await this.afAuth.auth.createUserWithEmailAndPassword(username + "@social-app.com", password)
 			console.log(res)
+			this.showAlert("Success!", "Welcome aboard!")
+			this.router.navigate(['/tabs'])
 		}catch(err){
+			this.showAlert("Error!", err.message)
 			console.dir(err)
 		}
-		
+	}
+
+	async showAlert(header: string, message: string){
+		const alert = await this.alert.create({
+			header,
+			message,
+			buttons: ["Ok"]
+		})
+
+		await alert.present()
 	}
 
 }
---
+++ src/app/tabs/tabs.page.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonTabs } from '@ionic/angular';
+
+@Component({
+  selector: 'app-tabs',
+  templateUrl: './tabs.page.html',
+  styleUrls: ['./tabs.page.scss']
+})
+export class TabsPage implements OnInit {
+
+  @ViewChild('tabs', {static: false}) tabs: IonTabs
+
+  constructor() { }
+  
+  ngOnInit() {
+
+    setTimeout(() => {
+      this.tabs.select('feed')
+    }, 500);
+
+  }
+
+}
