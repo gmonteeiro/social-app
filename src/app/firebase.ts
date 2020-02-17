1d8dd4cfafcac10e9df41731ad784b2b89be5f42
Gabriel Monteiro <gmonteeiro@gmail.com>
2020-02-17 01:04:53 -0300

    first commit

---
+++ src/app/firebase.ts
@@ -0,0 +1,11 @@
+const config = {
+    apiKey: "AIzaSyCUfoGTck8D_P_iPBMYP82WN3QeZsCYJI4",
+    authDomain: "social-app-e0202.firebaseapp.com",
+    databaseURL: "https://social-app-e0202.firebaseio.com",
+    projectId: "social-app-e0202",
+    storageBucket: "social-app-e0202.appspot.com",
+    messagingSenderId: "383867424045",
+    appId: "1:383867424045:web:98503dbdab915aaf0117a4"
+};
+
+export default config
